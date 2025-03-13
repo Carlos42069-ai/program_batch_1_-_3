c82cec78b3a6518913d89c555e9ab7788aae75ac
unknown <carlosroque117@gmail.com>
2025-03-13 23:57:22 +0800

    Added a code that checks if number is not a multiple of 10 and if condition is met then print number

--- disregarding_numbers_ending_in_zero.py
+++ disregarding_numbers_ending_in_zero.py
@@ -1,2 +1,4 @@
 #Make a code that skip numbers ending in 0
 for number in range(101): #Count 0 to 100
+    if number % 10 != 0: #Specific code that checks if number is not a multiple of 10
+        print(number) #Prints numbers
