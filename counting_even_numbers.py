1ea576623460d3a4cc4e7f4fcaae49eff7fa1e01
unknown <carlosroque117@gmail.com>
2025-03-13 23:12:59 +0800

    Getting 10 numbers from user and check for even numbers

--- counting_even_numbers.py
+++ counting_even_numbers.py
@@ -1,2 +1,8 @@
 #Defining even counter
 even_counter = 0
+
+#Loop runs 10 times to get 10 numbers
+for ten_number in range(10):
+    num = int(input(f"Enter number {ten_number+1}: "))
+    if num % 2 == 0: #Checking for even numbers
+        even_counter += 1
