955bc5d68676b6c142f3ad38ed48a5d983ee4bed
unknown <carlosroque117@gmail.com>
2025-03-13 22:42:35 +0800

    Setting the conditions and running the program without errors

--- finding_if_numbers_are_not_equal.py
+++ finding_if_numbers_are_not_equal.py
@@ -6,3 +6,6 @@ num2 = float(input("Enter your second number "))
 # Setting the Conditions to print Equal
 if num1 == num2:
     print("Equal")
+else:
+    print("The numbers input is not equal") #Adding else for another condition and to run the program correctly
+    
