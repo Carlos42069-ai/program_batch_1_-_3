ce94f3f9dc4c8ff92fde3cc9e50737ac5f40f4dc
unknown <carlosroque117@gmail.com>
2025-03-13 21:56:49 +0800

    Print each number in the range and running the program successfully

--- printing_bigger_number.py
+++ printing_bigger_number.py
@@ -1,8 +1,14 @@
 #Ask User to input two numbers
+
 num1 = int(input("Enter the first number: "))  
 num2 = int(input("Enter the second number: "))  
 
 #Swapping Values when num1 is bigger than num2
+
 if num1 > num2:
     num1, num2 = num2, num1  
+
 #Looping between numbers
+
+for num in range(num1 + 1, num2):  
+    print(num)  
