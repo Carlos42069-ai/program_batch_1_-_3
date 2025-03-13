04e3ce9fc17d80503b8ba19143ae30f6ffeeeac1
unknown <carlosroque117@gmail.com>
2025-03-14 00:12:32 +0800

    Asks user to enter a first and second number

--- first_number_powered_to_the_second_number.py
+++ first_number_powered_to_the_second_number.py
@@ -1,3 +1,5 @@
 #Ask User for first and second numnber
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
 #Define Variable name and its function
 #Print the Result
