2300379a1b58ba548eafd79810bad9936cde5cc0
unknown <carlosroque117@gmail.com>
2025-03-14 00:04:17 +0800

    Made a code that ask for numbers 1-10

--- first_number_minus_all_numbers.py
+++ first_number_minus_all_numbers.py
@@ -1,3 +1,13 @@
 #Ask user for numbers' 1-10
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
+num3 = float(input("Enter your third number "))
+num4 = float(input("Enter your fourth number "))
+num5 = float(input("Enter your fifth number "))
+num6 = float(input("Enter your sixth number "))
+num7 = float(input("Enter your seventh number "))
+num8 = float(input("Enter your eighth number "))
+num9 = float(input("Enter your nineth number "))
+num10 = float(input("Enter your tenth number "))
 #Define variable name and its function
 #Print the results
