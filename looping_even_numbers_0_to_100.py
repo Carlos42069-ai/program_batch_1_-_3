9dc6c61e7882ef0da5781b1dbf00a3e3d8e6ecd8
unknown <carlosroque117@gmail.com>
2025-03-13 23:40:24 +0800

    Added a code that prints all even numbers from 0 to 100

--- looping_even_numbers_0_to_100.py
+++ looping_even_numbers_0_to_100.py
@@ -1,2 +1,3 @@
 #Make a code that loops even numbers from 0 to 100
 for number in range(0, 101, 2): #Starts at zero and end at 100 and skips by 2
+    print(number) #Prints all even numbers from 0 to 100 
