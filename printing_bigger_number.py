6250463946b976e0b70585b9b26c74c85983b155
unknown <carlosroque117@gmail.com>
2025-03-13 22:09:20 +0800

    Psuedocode and code for asking user for first and second number

--- printing_bigger_number.py
+++ printing_bigger_number.py
@@ -1,14 +1,4 @@
-#Ask User to input two numbers
+#Ask for first and second number
 
-num1 = int(input("Enter the first number: "))  
-num2 = int(input("Enter the second number: "))  
-
-#Swapping Values when num1 is bigger than num2
-
-if num1 > num2:
-    num1, num2 = num2, num1  
-
-#Looping between numbers
-
-for num in range(num1 + 1, num2):  
-    print(num)  
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
