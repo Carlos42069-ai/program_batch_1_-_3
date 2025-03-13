7580a8383b62b19149c53f9160b8ed362ff72338
unknown <carlosroque117@gmail.com>
2025-03-13 23:18:19 +0800

    Added a pseuodocode for even_counter for better understanding and added a code that prints the total even numbers making a running program.

--- counting_even_numbers.py
+++ counting_even_numbers.py
@@ -5,4 +5,7 @@ even_counter = 0
 for ten_number in range(10):
     num = int(input(f"Enter number {ten_number+1}: "))
     if num % 2 == 0: #Checking for even numbers
-        even_counter += 1
+        even_counter += 1 #If there is an even number then even_counter goes up
+
+#Print total even numbers
+print("The total even numbers are: ", even_counter)
