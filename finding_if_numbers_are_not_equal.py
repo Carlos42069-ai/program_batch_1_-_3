9ad686f8945cd70d23ba7a76504348feb6a9674d
unknown <carlosroque117@gmail.com>
2025-03-13 22:24:32 +0800

    Asking user to enter first and second number and setting conditions to print Equal

---
+++ finding_if_numbers_are_not_equal.py
@@ -0,0 +1,7 @@
+#Ask user for a first and second number
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
+
+# Setting Conditions to print "Equal"
+if num1 == num2:
+    print("Equal")
