977da418f29c174dba98799575349f24a0849c2e
unknown <carlosroque117@gmail.com>
2025-03-13 21:54:38 +0800

    Swap values when num1 is bigger than num2

--- printing_bigger_number.py
+++ printing_bigger_number.py
@@ -3,5 +3,6 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))  
 
 #Swapping Values when num1 is bigger than num2
-
+if num1 > num2:
+    num1, num2 = num2, num1  
 #Looping between numbers
