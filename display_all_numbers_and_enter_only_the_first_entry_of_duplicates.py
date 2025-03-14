07fc4d854fbe391575eb1e09c630613341941e8d
unknown <carlosroque117@gmail.com>
2025-03-14 20:55:20 +0800

    Taking 10 numbers from user and returning them as a list.

--- display_all_numbers_and_enter_only_the_first_entry_of_duplicates.py
+++ display_all_numbers_and_enter_only_the_first_entry_of_duplicates.py
@@ -1,3 +1,5 @@
 #Ask user for 10 numbers to put in the list
+def input_numbers():
+    return [int(input("Enter a number: ")) for _ in range(10)]
 #Keep only the first occurrence of each number.
 #Print the final list of numbers.
