2a98fdc2cf25faa63459b271c9a40b7e003b5c94
unknown <carlosroque117@gmail.com>
2025-03-14 22:23:31 +0800

    Pseudocode for program number 5 in batch 4.

---
+++ calculate_average_after_invalid_input.py
@@ -0,0 +1,5 @@
+#Continuously ask the user to input a number.
+#If the input is invalid (non-numeric), stop asking for input.
+#Store valid numbers in a list.
+#Calculate the average of the numbers.
+#Display the average.
