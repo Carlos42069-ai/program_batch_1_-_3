036ba3ec488bd395538acae126095e5055f980a0
unknown <carlosroque117@gmail.com>
2025-03-14 21:48:49 +0800

    Pseudocode for program number 2 in batch 5.

---
+++ most_frequent_number.py
@@ -0,0 +1,4 @@
+#Continuously ask the user to input a number.
+#If the input is invalid (non-numeric), stop asking for input.
+#Store valid numbers in a list.
+#Identify and display the number with the highest count of duplicates.
