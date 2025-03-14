457dc92285b1a33a780bff1d020d8cbb887b112d
unknown <carlosroque117@gmail.com>
2025-03-14 20:01:02 +0800

    Added a code for asking the 10 numbers.

--- printing_the_sum_of_all_numbers.py
+++ printing_the_sum_of_all_numbers.py
@@ -1,3 +1,14 @@
 #Ask user for 10 numbers
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
+num3 = float(input("Enter your third number "))
+num4 = float(input("Enter your fourth number "))
+num5 = float(input("Enter your fifth number "))
+num6 = float(input("Enter your sixth number "))
+num7 = float(input("Enter your seventh number "))
+num8 = float(input("Enter your eighth number "))
+num9 = float(input("Enter your nineth number "))
+num10 = float(input("Enter your tenth number "))
+
 #Make a variable name and its function
 #Print the sum of all numbers
