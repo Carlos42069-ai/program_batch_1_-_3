dd1cff19f2f2a65769e1255d39fd4782525e9f75
unknown <carlosroque117@gmail.com>
2025-03-14 20:39:13 +0800

    Define a function to take 10 numbers as input and store them in a list.

--- display_numbers_without_duplicate.py
+++ display_numbers_without_duplicate.py
@@ -1,5 +1,8 @@
 #Ask user for 10 numbers to put in the list
+def input_ten_numbers():
+    return [int(input("Enter a number: ")) for numbers in range(10)]
 #Define a function to find and return numbers that appear only once in the list.
+
 #Get user input by calling the input function.
 #Identify unique numbers using the second function.
 #Print the unique numbers.
