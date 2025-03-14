1fa8ce6977d8cf308f5c30967f82f48ddac85ec4
unknown <carlosroque117@gmail.com>
2025-03-14 18:29:26 +0800

    Added a code that asks the user for numbers and made the first condition

--- when_number_is_not_equal_print_not_equal.py
+++ when_number_is_not_equal_print_not_equal.py
@@ -1,3 +1,8 @@
 #Ask user for a first and second number
-
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 #Make a loop and set conditions that print "Not Equal" when numbers entered are not equal
+if num1 != num2:
+    print("Not Equal")
+
+
