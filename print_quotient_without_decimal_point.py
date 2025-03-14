8482ec4e808aeb51aa168f1709cad7f2590236e1
unknown <carlosroque117@gmail.com>
2025-03-14 19:33:12 +0800

    Added a code for the first pseudocode and setting the condition for the if statenent.

--- print_quotient_without_decimal_point.py
+++ print_quotient_without_decimal_point.py
@@ -1,3 +1,8 @@
 #Ask user for two numbers
+num1 = int(input("Enter your first number: ")) #using int for no decimal point
+num2 = int(input("Enter your second number: "))
 #Setting if else conditions
+if num2 !=0:
+    quotient = num1 // num2
+    
 #Printing the quotient 
