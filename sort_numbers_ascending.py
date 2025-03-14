38ea8228248015fba41ac2715305a7bf1a8ebf17
unknown <carlosroque117@gmail.com>
2025-03-14 21:31:52 +0800

    Creating a code the sorts the numbers from lowest to highest and as stated the hint is sort(), using this function makes the program easier to code as the following programs in batch 3 has the same pattern.

--- sort_numbers_ascending.py
+++ sort_numbers_ascending.py
@@ -3,3 +3,20 @@
 #Store valid numbers in a list.
 #After input ends, sort the numbers from lowest to highest.
 #Display the sorted numbers.
+def getting_the_numbers():
+    numbers = []
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Stopping program.")
+            break
+    
+    if numbers:
+        numbers.sort()
+        print("Numbers in order:", numbers)
+    else:
+        print("No valid numbers were entered.")
+        
+getting_the_numbers()
