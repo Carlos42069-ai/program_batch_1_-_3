6549dd1357d2405c0cef07aae6362ca32c648418
unknown <carlosroque117@gmail.com>
2025-03-14 20:35:34 +0800

    Pseudocode for first program in batch 3.

---
+++ display_numbers_without_duplicate.py
@@ -0,0 +1,5 @@
+#Ask user for 10 numbers to put in the list
+#Define a function to find and return numbers that appear only once in the list.
+#Get user input by calling the input function.
+#Identify unique numbers using the second function.
+#Print the unique numbers.
