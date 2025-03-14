92122090f41b4dac1797196b87b4b1ead09f0805
unknown <carlosroque117@gmail.com>
2025-03-14 19:19:30 +0800

    Added a code to print the quotient and setting a condition for else statement.

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -7,3 +7,7 @@ if num2 !=0:
     quotient = num1 / num2
 
 #Print the quotient of two numbers
+    print("The quotient of two numbers: ", quotient)
+
+else:
+    print("Please re-enter your first and second number")
