0599d5c564b7770add2153067d4eee02151740db
unknown <carlosroque117@gmail.com>
2025-03-14 22:12:40 +0800

    Pseudocode for program number 4 in batch 4.

---
+++ sort_numbers_descending.py
@@ -0,0 +1,5 @@
+#Continuously ask the user to input a number.
+#If the input is invalid (non-numeric), stop asking for input.
+#Store valid numbers in a list.
+#Sort the numbers from highest to lowest.
+#Display the sorted numbers.
