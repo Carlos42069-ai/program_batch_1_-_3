43d22342be281250a038494bda11326c88d2231f
unknown <carlosroque117@gmail.com>
2025-03-14 21:22:56 +0800

    Creating a code following the pseudocode for this program.

--- finding_lowest_number.py
+++ finding_lowest_number.py
@@ -2,3 +2,19 @@
 #If the input is invalid (non-numeric), stop asking for the input.
 #Store valid numbers in a list.
 #After input ends, display the lowest number from the list.
+def getting_the_numbers():
+    numbers = []
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input detected. Stopping the program.")
+            break
+    
+    if numbers:
+        print("Lowest number:", min(numbers))
+    else:
+        print("No valid numbers were entered.")
+
+getting_the_numbers()
