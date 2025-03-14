05c5faedc805f01a3ff904b645941fadc62423d0
unknown <carlosroque117@gmail.com>
2025-03-14 19:09:49 +0800

    Changed int to float for versatility, changed the wrong function variable, and added a code for printing the producct

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,7 +1,9 @@
 #Ask the user for two numbers
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
 
 #Define variables and its function
-product_of_numbers = num1 ** num2
+product_of_numbers = num1 * num2
+
 #Print the product
+print("The product of the two numbers are: ", product_of_numbers)
