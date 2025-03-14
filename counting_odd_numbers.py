1ecde18d9ecdd4feaafe2c234b250f4200b5268c
unknown <carlosroque117@gmail.com>
2025-03-14 18:45:08 +0800

    Corrected the error and added a code that prints the total odd number

--- counting_odd_numbers.py
+++ counting_odd_numbers.py
@@ -3,6 +3,7 @@ odd_number_count = 0
 #Set conditions for counting odd number
 for ten_numbers in range(10):
     num = int(input(f"Enter a number {ten_numbers+1}:"))
-     if num % 2 != 0
+    if num % 2 != 0: #first commit have extra space and made an error, added a colon to run the program correctly
         odd_number_count +=1
 #Print the total odd number counted
+print("The total odd number is: ", odd_number_count)
