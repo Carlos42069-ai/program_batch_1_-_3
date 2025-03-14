aee4b4abb0090902e5fa78239f2e3c4f9fe14e1d
unknown <carlosroque117@gmail.com>
2025-03-14 20:07:56 +0800

    Made a code that asks the user for two numbers.

--- print_the_sum_of_two_numbers.py
+++ print_the_sum_of_two_numbers.py
@@ -1,3 +1,7 @@
 #Ask the user for two numbers
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
+
 #Add a variable name with its function
+
 #Print the sum of the two numbers
