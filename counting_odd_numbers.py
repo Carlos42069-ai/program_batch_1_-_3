a2775bf7a6a11e6d0fb519d40d84c2c4dcda490a
unknown <carlosroque117@gmail.com>
2025-03-14 18:40:43 +0800

    Made a code that asks for 10 numbers and set conditions for counting odd numbers

--- counting_odd_numbers.py
+++ counting_odd_numbers.py
@@ -1,3 +1,8 @@
 #Define variable name for counting odd number
+odd_number_count = 0
 #Set conditions for counting odd number
+for ten_numbers in range(10):
+    num = int(input(f"Enter a number {ten_numbers+1}:"))
+     if num % 2 != 0
+        odd_number_count +=1
 #Print the total odd number counted
