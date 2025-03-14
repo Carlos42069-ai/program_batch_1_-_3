12387d9c8c451160bbe4af9d720be370f767ae10
unknown <carlosroque117@gmail.com>
2025-03-14 21:18:43 +0800

    Pseudocode for program number 4 in batch 3.

---
+++ finding_lowest_number.py
@@ -0,0 +1,4 @@
+#Continuously ask the user to input a number.
+#If the input is invalid (non-numeric), stop asking for the input.
+#Store valid numbers in a list.
+#After input ends, display the lowest number from the list.
