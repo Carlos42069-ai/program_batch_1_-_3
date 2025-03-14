c1883709d5b862ac6f4a71357b2a93dfde1e0d8c
unknown <carlosroque117@gmail.com>
2025-03-14 19:06:34 +0800

    Added a code for the first two pseudocode

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,3 +1,7 @@
 #Ask the user for two numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
 #Define variables and its function
+product_of_numbers = num1 ** num2
 #Print the product
