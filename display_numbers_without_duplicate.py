3b7e722c53c383f4f3e3f53b03fd31ae31968d61
unknown <carlosroque117@gmail.com>
2025-03-14 20:43:25 +0800

    Defining a function that returns number that appears once

--- display_numbers_without_duplicate.py
+++ display_numbers_without_duplicate.py
@@ -1,7 +1,9 @@
 #Ask user for 10 numbers to put in the list
 def input_ten_numbers():
-    return [int(input("Enter a number: ")) for numbers in range(10)]
+    return [int(input("Enter a number: ")) for numero in range(10)]
 #Define a function to find and return numbers that appear only once in the list.
+def finding_unique(numbers):
+        return [num for num in numbers if numbers.count(num) == 1]
 
 #Get user input by calling the input function.
 #Identify unique numbers using the second function.
