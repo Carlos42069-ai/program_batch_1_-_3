57c57b570573f71d0f2e88c2769683001f95e076
unknown <carlosroque117@gmail.com>
2025-03-14 19:18:41 +0800

    Added code for asking the user for two numbers and setting condition for the if statement.

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -1,3 +1,9 @@
 #Ask user for a first and second number
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
 #Make an if else statement conditions for the objective
+if num2 !=0:
+    quotient = num1 / num2
+
 #Print the quotient of two numbers
