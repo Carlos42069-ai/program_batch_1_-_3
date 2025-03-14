3f1b12f95a05db254d9530597493df5f4fa7deb9
unknown <carlosroque117@gmail.com>
2025-03-14 19:50:04 +0800

    Made a code that asks the first and second number and set the conditions for the else if statements.

--- printing_the_remainder_of_two_numbers.py
+++ printing_the_remainder_of_two_numbers.py
@@ -1,2 +1,10 @@
 #Ask user for two numbers
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
 #Setting if else conditions and printing the remainder
+if num2 !=0:
+    remainder = num1 % num2
+    print("The remainder is: ", remainder)
+
+else:
+    print("Please re-enter your two numbers")
