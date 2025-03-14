65358c080a48f6fb9e180c27e1d1ea77f494a658
unknown <carlosroque117@gmail.com>
2025-03-14 21:14:50 +0800

    Defined a variable for getting the numbers and made a while loop based from the pseudocode.

--- check_the_number_uniqueness.py
+++ check_the_number_uniqueness.py
@@ -3,3 +3,18 @@
 #Store valid numbers in a list.
 #After each input, check if the number has appeared before.
 # Display "Unique" if the number appears for the first time, otherwise display "Duplicate".
+def getting_the_numbers():
+    numbers = []
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            if num in numbers:
+                print("Duplicate")
+            else:
+                print("Unique")
+                numbers.append(num)
+        except ValueError:
+            print("Invalid input detected. Closing the program.")
+            break
+
+getting_the_numbers()
