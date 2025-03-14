714147f075e0c44da9129b6accb23bb70337be84
unknown <carlosroque117@gmail.com>
2025-03-14 19:58:13 +0800

    Added a code that asks for the first and second number, prints the smaller number using min function.

--- printing_the_smaller_number.py
+++ printing_the_smaller_number.py
@@ -1,2 +1,6 @@
 #Ask the first two numbers
+num1 = float(input("Enter your first number "))
+num2 = float(input("Enter your second number "))
+
 #Printing the smaller number
+print("The smaller number is: ", min(num1,num2))
