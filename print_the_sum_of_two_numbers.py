ce19fbb9397a988c288d82bbbb684017d7cc2339
unknown <carlosroque117@gmail.com>
2025-03-14 20:09:39 +0800

    Added a print function that prints out the sum of the two numbers.

--- print_the_sum_of_two_numbers.py
+++ print_the_sum_of_two_numbers.py
@@ -3,5 +3,7 @@ num1 = float(input("Enter your first number "))
 num2 = float(input("Enter your second number "))
 
 #Add a variable name with its function
+sum = num1 + num2
 
 #Print the sum of the two numbers
+print("The sum is: ", sum)
