617d2cfcf038e28267f6f847226074b378d99a81
unknown <carlosroque117@gmail.com>
2025-03-14 21:08:30 +0800

    Pseudocode for program number 3 in batch 3.

---
+++ check_the_number_uniqueness.py
@@ -0,0 +1,5 @@
+#Continuously ask the user to input a number.
+#If the input is invalid (non-numeric), stop asking for input.
+#Store valid numbers in a list.
+#After each input, check if the number has appeared before.
+# Display "Unique" if the number appears for the first time, otherwise display "Duplicate".
